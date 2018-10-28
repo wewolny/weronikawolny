4a69bfbc5f88b742f80a1d96033ed9118b79a458
Weronika Wolny <44445959+wewolny@users.noreply.github.com>
2018-10-28 18:34:47 +0100

    Add files via upload

---
+++ problem004.js
@@ -0,0 +1,11 @@
+﻿function getTotalNumberOfNewYaersEveSundays(a, b){
+  var l=0;
+  var d = new Date(2006,0,1);
+  for( var i = a; i<= b; ++i)
+    {
+      d = new Date (i,0,1);
+      if((new Date(i,0,1)).getDay()===0)
+        ++l;
+    }
+  return l;
+}

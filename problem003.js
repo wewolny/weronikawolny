58f0f3d6575b1dae072d71d2382fcbc2438904c6
Weronika Wolny <44445959+wewolny@users.noreply.github.com>
2018-10-28 17:39:57 +0100

    Update problem003.js

--- problem003.js
+++ problem003.js
@@ -1,4 +1,7 @@
-﻿function isLeapYear(a){		 
-    if(a%4===0)
-        return true;
-    else return false;}
+function isLeapYear(a){		 
+    if(a%4===0&&a%100!=0)
+      return true;
+    if(a%400===0)
+      return true;
+  return false;
+      }

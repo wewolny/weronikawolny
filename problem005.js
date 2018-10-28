1790374c391a657441415dfad0746103e5e6b7cc
Weronika Wolny <44445959+wewolny@users.noreply.github.com>
2018-10-28 17:48:15 +0100

    Update problem005.js

--- problem005.js
+++ problem005.js
@@ -1,5 +1,5 @@
-﻿function strangeSum(a,b){
-if(typeof a ==='number' && typeof b ==='number')
+function strangeSum(a,b){
+if(typeof a ==='number' && typeof b ==='number' && a%1===0 && b%1===0)
 {
     if(a===b)
         return 3*(a+b);

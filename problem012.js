7703b535531c7757b7946fb90475d8869cb187d8
Weronika Wolny <44445959+wewolny@users.noreply.github.com>
2018-11-12 16:27:44 +0100

    Add files via upload

---
+++ problem012.js
@@ -0,0 +1,13 @@
+﻿function objectsDiff(a,b){
+  var tab = [];
+  for(var property in a){
+    if(!(property in b))
+      tab.push(property);
+  }
+  
+  for(var property in b){
+    if(!(property in a))
+      tab.push(property);
+  }
+  return tab;
+}

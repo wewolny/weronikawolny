6b11a4cdbdb20684e4638ef33a2c93c1a2a7fa00
Weronika Wolny <44445959+wewolny@users.noreply.github.com>
2018-11-12 16:28:26 +0100

    Update problem014.js

--- problem014.js
+++ problem014.js
@@ -1,17 +1,12 @@
 ﻿var Vectors = {
   sum: function(a,b){
     for(var i=0; i<a.length; i++)
-      if(typeof a[i]!='number'||typeof b[i]!='number')
-        return false;
     if(a.length!=3||b.length!=3)
       return false;
     var tab = [(a[0]+b[0]),(a[1]+b[1]),(a[2]+b[2])]
     return tab;
   },
   mulByScalar: function(a,b){
-     for(var i=0; i<a.length; i++)
-      if(typeof a[i]!='number')
-        return false;
     if(a.length!=3||typeof b!='number')
       return false;
     var tab = [];

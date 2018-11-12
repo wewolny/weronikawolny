56d195cba7939228e321029d9d845c428f4ef205
Weronika Wolny <44445959+wewolny@users.noreply.github.com>
2018-11-12 13:20:06 +0100

    Add files via upload

---
+++ problem011.js
@@ -0,0 +1,12 @@
+﻿function deepFindAndCount(a,b){
+  var count = 0;
+  for(var i=0; i<a.length; i++){
+    if(Array.isArray(a[i]))
+      count += deepFindAndCount(a[i],b);
+      else
+        if(a[i]===b)
+          count++;
+  }
+  
+  return count;
+}

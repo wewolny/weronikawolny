862d393fc8db9eadeea8fbd8b543af4b130f34a8
Weronika Wolny <44445959+wewolny@users.noreply.github.com>
2018-11-12 15:34:34 +0100

    Add files via upload

---
+++ problem015.js
@@ -0,0 +1,20 @@
+﻿stack =[];
+var Stack = {
+  push: function(a,b){
+    if(isNaN(b))
+      b=1;
+    for(var i=0; i<b-1; i++)
+      stack.push(null);
+    stack.push(a);
+    return stack;
+    
+  },
+  pop: function(a){
+    if(isNaN(a))
+      a=1;
+    var l;
+    for(var i=0; i<a; i++)
+      l=stack.pop(a);
+    return l;
+  }
+}

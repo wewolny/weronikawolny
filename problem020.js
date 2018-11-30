da9840be9989b3ea5b05ce3ebcaa4dc3e89fcaa8
Weronika Wolny <44445959+wewolny@users.noreply.github.com>
2018-11-30 22:21:45 +0100

    Add files via upload

---
+++ problem020.js
@@ -0,0 +1,8 @@
+﻿function Square(a) {
+    this.a = a;}
+
+Square.prototype.getArea = function() {
+    return (this.a * this.a);}
+
+Square.prototype.getPerimeter = function() {
+    return (4 * this.a);}
